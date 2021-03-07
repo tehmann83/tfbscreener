46b2c56abc141892f30d781f7b48059eafc986fa
Thomas Ehmann <ehmannth@gmail.com>
2021-03-07 12:33:39 +0100

    Add User API Routes & JWT Authentication

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 // Connect Database
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send(`API running`));
 
 // Define Routes
