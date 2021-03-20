38ad75aaa0508af3f274020411f42e7d68b040e8
Thomas Ehmann <ehmannth@gmail.com>
2021-03-20 19:02:33 +0100

    Add get all profiles/ user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -111,4 +111,38 @@ router.post(
 	}
 );
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+	try {
+		const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+		res.json(profiles);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server error');
+	}
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+	try {
+		const profile = await Profile.findOne({
+			user: req.params.user_id
+		}).populate('user', ['name', 'avatar']);
+
+		if (!profile) res.status(400).json({ msg: 'Profile not found' });
+
+		res.json(profile);
+	} catch (error) {
+		console.error(error.message);
+		if (error.kind === 'ObjectId') {
+			res.status(400).json({ msg: 'Profile not found' });
+		}
+		res.status(500).send('Server error');
+	}
+});
+
 module.exports = router;
