8ae0faeada0f11b2e53d1b3636c00e4da78864d3
Thomas Ehmann <ehmannth@gmail.com>
2021-03-20 20:38:45 +0100

    Add delete experience route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 const { compare } = require('bcryptjs');
+const { route } = require('./auth');
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -218,5 +219,28 @@ router.put(
 		}
 	}
 );
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete profile experience
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id });
+
+		// Get remove index
+		const removeIndex = profile.experience
+			.map(item => item.id)
+			.indexOf(req.params.exp_id);
+
+		profile.experience.splice(removeIndex, 1);
+
+		await profile.save();
+
+		res.json(profile);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server error');
+	}
+});
 
 module.exports = router;
