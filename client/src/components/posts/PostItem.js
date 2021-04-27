a9dc223d54a71e44be71a3a6007dd7de8bc35f3e
Thomas Ehmann <ehmannth@gmail.com>
2021-04-27 17:28:56 +0200

    Add addPost and deletePost

---
+++ client/src/components/posts/PostItem.js
@@ -0,0 +1,84 @@
+import { addLike, deletePost, removeLike } from '../../actions/post';
+import {
+	faThumbsDown,
+	faThumbsUp,
+	faTimes
+} from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import Moment from 'react-moment';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { connect } from 'react-redux';
+
+const PostItem = ({
+	addLike,
+	removeLike,
+	deletePost,
+	auth,
+	post: { _id, text, name, avatar, user, likes, comments, date }
+}) => {
+	return (
+		<div className="post bg-white p-1 my-1">
+			<div>
+				<Link to={`/profile/${user}`}>
+					<img className="round-img" src={avatar} alt="" />
+					<h4>{name}</h4>
+				</Link>
+			</div>
+			<div>
+				<p className="my-1">{text}</p>
+				<p className="post-date">
+					Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
+				</p>
+				<button
+					type="button"
+					className="btn btn-light"
+					onClick={e => addLike(_id)}
+				>
+					<FontAwesomeIcon icon={faThumbsUp} />{' '}
+					{likes.length > 0 && <span>{likes.length}</span>}
+				</button>
+				<button
+					type="button"
+					className="btn btn-light"
+					onClick={e => removeLike(_id)}
+				>
+					<FontAwesomeIcon icon={faThumbsDown} />{' '}
+				</button>
+				<Link to={`/post/${_id}`} className="btn btn-primary">
+					Discussion{' '}
+					{comments.length > 0 && (
+						<span className="comment-count">{comments.length}</span>
+					)}
+				</Link>
+				{!auth.loading && user === auth.user._id && (
+					<button
+						onClick={e => deletePost(_id)}
+						type="button"
+						className="btn btn-danger"
+					>
+						<FontAwesomeIcon icon={faTimes} />
+					</button>
+				)}
+			</div>
+		</div>
+	);
+};
+
+PostItem.propTypes = {
+	post: PropTypes.object.isRequired,
+	auth: PropTypes.object.isRequired,
+	addLike: PropTypes.func.isRequired,
+	removeLike: PropTypes.func.isRequired,
+	deletePost: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+	auth: state.auth
+});
+
+export default connect(mapStateToProps, { addLike, removeLike, deletePost })(
+	PostItem
+);
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,15 +1,34 @@
 import React, { useEffect } from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import PostForm from './PostForm';
+import PostItem from './PostItem';
 import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 import { connect } from 'react-redux';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { getPosts } from '../../actions/post';
 
 const Posts = ({ getPosts, post: { posts, loading } }) => {
 	useEffect(() => {
 		getPosts();
 	}, [getPosts]);
-	return <div></div>;
+	return loading ? (
+		Spinner
+	) : (
+		<>
+			<h1 className="large text-primary">Posts</h1>
+			<p className="lead">
+				<FontAwesomeIcon icon={faUser} />
+			</p>
+			<PostForm />
+			<div className="posts">
+				{posts.map(post => (
+					<PostItem key={post._id} post={post} />
+				))}
+			</div>
+		</>
+	);
 };
 
 Posts.propTypes = {
